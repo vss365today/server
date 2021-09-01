694c541a61b35f1d6868e3f606ab8726d05dd71a
Caleb Ely <le717@users.noreply.github.com>
2021-08-31 22:15:38 -0400

    Pull the latest changes before building

--- build-containers.sh
+++ build-containers.sh
@@ -1,13 +1,16 @@
 # Build the API
 cd ../api
+git pull
 docker build -t vss365today-api:latest .
 
 # Build the web interface
 cd ../web
+git pull
 docker build -t vss365today-web:latest .
 
 # Build the finder
 cd ../finder
+git pull
 docker build -t vss365today-finder:latest .
 
 # Return to the server directory
